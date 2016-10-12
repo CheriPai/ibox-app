2e40da2abda0fbea2b17e542ace0832b71e64276
dat <datdo@rocketmail.com>
2016-10-12 09:43:48 -0700

    Finish all unit tests

--- src/test/java/edu/csupomona/cs585/ibox/GoogleDriveFileSyncManagerTest.java
+++ src/test/java/edu/csupomona/cs585/ibox/GoogleDriveFileSyncManagerTest.java
@@ -85,6 +85,86 @@ public class GoogleDriveFileSyncManagerTest {
 	}
 
 	@Test()
+	public void testUpdateFile() throws IOException {
+		java.io.File localFile = new java.io.File("~/Java/ibox-app/watch/test");
+		File file = new File();
+		file.setId("test");
+		file.setTitle("test");
+
+		FileList filelist = new FileList();
+		java.util.List<File> list = new java.util.ArrayList<File>();
+		list.add(file);
+		filelist.setItems(list);
+
+		Files files = mock(Files.class);
+		Files.Update update = mock(Files.Update.class);
+		List request = mock(List.class);
+
+		when(mockedDrive.files()).thenReturn(files);
+		when(files.list()).thenReturn(request);
+		when(request.execute()).thenReturn(filelist);
+		when(files.update(Mockito.any(String.class), Mockito.any(File.class), Mockito.any(FileContent.class))).thenReturn(update);
+		when(update.execute()).thenReturn(file);
+				
+		fileSyncManager.updateFile(localFile);
+
+		verify(update).execute();
+		assertEquals("File ID: test\n", outContent.toString());
+	}
+
+	@Test()
+	public void testUpdateFileFileIdNull() throws IOException {
+		java.io.File localFile = new java.io.File("~/Java/ibox-app/watch/test");
+		File file = new File();
+		file.setId("DNE");
+		file.setTitle("DNE");
+
+		FileList filelist = new FileList();
+		java.util.List<File> list = new java.util.ArrayList<File>();
+		list.add(file);
+		filelist.setItems(list);
+
+		Files files = mock(Files.class);
+		Files.Insert insert = mock(Files.Insert.class);
+		List request = mock(List.class);
+
+		when(mockedDrive.files()).thenReturn(files);
+		when(files.list()).thenReturn(request);
+		when(request.execute()).thenReturn(filelist);
+		when(files.insert(Mockito.any(File.class), Mockito.any(FileContent.class))).thenReturn(insert);
+		when(insert.execute()).thenReturn(file);
+				
+		fileSyncManager.updateFile(localFile);
+		verify(insert).execute();
+		assertEquals("File ID: DNE\n", outContent.toString());
+	}
+
+	@Test(expected=IOException.class)
+	public void testUpdateFileIOException() throws IOException {
+		java.io.File localFile = new java.io.File("~/Java/ibox-app/watch/test");
+		File file = new File();
+		file.setId("test");
+		file.setTitle("test");
+
+		FileList filelist = new FileList();
+		java.util.List<File> list = new java.util.ArrayList<File>();
+		list.add(file);
+		filelist.setItems(list);
+
+		Files files = mock(Files.class);
+		Files.Update update = mock(Files.Update.class);
+		List request = mock(List.class);
+
+		when(mockedDrive.files()).thenReturn(files);
+		when(files.list()).thenReturn(request);
+		when(request.execute()).thenReturn(filelist);
+		when(files.update(Mockito.any(String.class), Mockito.any(File.class), Mockito.any(FileContent.class))).thenReturn(update);
+		when(update.execute()).thenThrow(new IOException());
+				
+		fileSyncManager.updateFile(localFile);
+	}
+
+	@Test()
 	public void testDeleteFile() throws IOException {
 		java.io.File localFile = new java.io.File("~/Java/ibox-app/watch/test");
 
@@ -130,6 +210,31 @@ public class GoogleDriveFileSyncManagerTest {
 		when(mockedDrive.files()).thenReturn(files);
 		when(files.list()).thenReturn(request);
 		when(request.execute()).thenReturn(filelist);
+		
+		fileSyncManager.deleteFile(localFile);
+	}
+
+	@Test(expected=IOException.class)
+	public void testDeleteFileIOException() throws IOException {
+		java.io.File localFile = new java.io.File("~/Java/ibox-app/watch/test");
+
+		File file = new File();
+		file.setId("test");
+		file.setTitle("test");
+
+		FileList filelist = new FileList();
+		java.util.List<File> list = new java.util.ArrayList<File>();
+		list.add(file);
+		filelist.setItems(list);
+
+		Files files = mock(Files.class);
+		List request = mock(List.class);
+		Files.Delete delete = mock(Files.Delete.class);
+
+		when(mockedDrive.files()).thenReturn(files);
+		when(files.list()).thenReturn(request);
+		when(request.execute()).thenReturn(filelist);
+		when(files.delete(Mockito.any(String.class))).thenThrow(new IOException());
 		
 		fileSyncManager.deleteFile(localFile);
 	}
